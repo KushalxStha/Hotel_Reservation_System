70f2f5687be7d677008391bd3cb2b5221e91a633
Kushal Shrestha <shresthakushal61@gmail.com>
2024-08-14 20:18:36 +0545

    Loading Driver & Creating Connection

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,23 @@
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
 public class Main {
+    private static final String url="jdbc:mysql://localhost:3306/hotel_db";
+    private static final String user="root";
+    private static final String password="Mysql@24";
+
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        try{
+            Class.forName("com.mysql.cj.jdbc.Driver");
+        }catch (ClassNotFoundException e){
+            e.getStackTrace();
+        }
+
+        try{
+            Connection connection = DriverManager.getConnection(url, user, password);
+        }catch (SQLException e){
+            e.getStackTrace();
+        }
     }
 }
